dd295ad26fb0febc3d3f36b7faaec70a6c444094
fabianex87 <fabian587@hotmail.com>
2022-03-04 22:19:12 +0100

    add file gitPush.js v.2

--- gitPush.js
+++ gitPush.js
@@ -6,9 +6,19 @@ git commit -m "first commit"
 git push
 
 qua ti consiglia di fare
-git push --set-upstream origin feature/fabian = git push -u origin feature/fabian
+git push --set-upstream origin feature/fab = git push -u origin feature/fabian
+ma ti chiederà una sola volta di fare git push --set-upstream origin branch, 
+le prossime volte ti basterà fare git push
 
-ma che è la stessa cosa di fare
+invece se fai
 git push origin feature/fabian
+ogni volta che fai git push, ti chiederà di fare git push --set-upstream
+questo è dovuto a che bel file config, non viene creato la tua nuova branch
 
+invce con -u viene creato
+[branch "feature/fab"]
+	remote = origin
+	merge = refs/heads/feature/fabian
+
+ed ecco il motivo che conviene fare -u (--set-upstream)
 */
