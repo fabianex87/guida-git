23411e810f7a928420fbb75e42a6c471060a3739
fabianex87 <fabian587@hotmail.com>
2022-03-04 22:14:04 +0100

    add file gitPush.js

---
+++ gitPush.js
@@ -0,0 +1,14 @@
+/*
+
+quando si crea il branch in locale e fai
+git add . 
+git commit -m "first commit"
+git push
+
+qua ti consiglia di fare
+git push --set-upstream origin feature/fabian = git push -u origin feature/fabian
+
+ma che è la stessa cosa di fare
+git push origin feature/fabian
+
+*/
