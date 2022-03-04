f11f343c813d72849f5a28328a3b4c0440160ab5
fabianex87 <fabian587@hotmail.com>
2022-03-04 22:36:12 +0100

    add gitPull.js

---
+++ gitPull.js
@@ -0,0 +1,18 @@
+/*
+
+master
+ - feature/fabian 
+ - feature/fab 
+
+ho fatto diverse modifiche e ho pushato
+per esmepio ho fatto l'ultima modiifca su feature/fab e l'ho mergiato al master
+
+adesso switcho a feature/fabian 
+e faccio git pull ma me ne sono reso conto che non si porta le ultime modifiche 
+come se feature/fab non fosse stato creato dal master
+
+e quindi ho dovuto fare
+git pull origin master
+
+
+*/
